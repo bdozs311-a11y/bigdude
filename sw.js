dbacc5b7df3df4fe3d27484019a2bc6ee5376722
bdozs311-a11y <bdozs311@gmail.com>
2026-09-08 20:02:30 +1000

    Publish My Sounds iPhone app

---
+++ sw.js
@@ -0,0 +1,1 @@
+const cache='my-sounds-shell-v1';const files=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(cache).then(c=>c.addAll(files))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
